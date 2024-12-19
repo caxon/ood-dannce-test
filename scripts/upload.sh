bbc75416aa69c7ded25bc0120aef8ac2f777e1f2
caxon <christopher_axon@harvard.edu>
2024-12-18 19:07:28 -0500

    updates

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # upload the dev package from a local computer to the cluster
+echo "TIMING - upload at $(date)"
 
 rsync -avhP ./ caxon@cannon:~/.fasrcood/dev/ood_dannce_test/ --delete
 
